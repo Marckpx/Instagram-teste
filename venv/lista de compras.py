c97774f5a30509ffae36222712a89b184cd8097d
Marckpx <marquinho.asp@gmail.com>
2020-12-06 12:59:12 -0300

    Teste do Instagram, login

---
+++ venv/lista de compras.py
@@ -0,0 +1,6 @@
+# Contador de lista do mercado
+print('{} ==== {} ==== {}'.format('Produtos', 'Quantidade', 'R$'))
+p = str(input('')).cont(1, 59)
+qt = int(input(''))
+v = float(input(''))
+print('{}{} ==== {}{} ==== {}{}'.format('p> ',p,'qt> ',qt,'R$ ',v))
